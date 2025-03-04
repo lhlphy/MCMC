7113d83c6f65f1d51bf3c9d2f2e3d4b6b82fd1b4
dell <lhlphy@qq.com>
2025-03-04 13:34:39 +0800

    K2

--- core/test.py
+++ core/test.py
@@ -1,3 +1,6 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import numpy as np
 import matplotlib.pyplot as plt
 from core.analytical_model import *
@@ -17,21 +20,21 @@ if __name__ == '__main__':
     F_ellip = F_ellip(Theta_array, alpha_ellipse)
     print(f'time1: {time.time() - t1}')
     
-    # # subplot, plot each function as a subplot, so 4 subplots in total
-    # fig, axs = plt.subplots(2, 2, figsize=(10, 10))
-    # axs[0, 0].plot(Theta_array, F_thermal)
-    # axs[0, 0].set_title("F_thermal")
-    # axs[0, 0].set_ylim([0, max(F_thermal)*1.2])
-    # axs[0, 1].plot(Theta_array, F_specular)
-    # axs[0, 1].set_title("F_specular")
-    # axs[1, 0].plot(Theta_array, F_Doppler)
-    # axs[1, 0].set_title("F_Doppler")
-    # axs[1, 1].plot(Theta_array, F_ellip)
-    # axs[1, 1].set_title("F_ellip")
-    # plt.show()
+    # subplot, plot each function as a subplot, so 4 subplots in total
+    fig, axs = plt.subplots(2, 2, figsize=(10, 10))
+    axs[0, 0].plot(Theta_array, F_thermal)
+    axs[0, 0].set_title("F_thermal")
+    axs[0, 0].set_ylim([0, max(F_thermal)*1.2])
+    axs[0, 1].plot(Theta_array, F_specular)
+    axs[0, 1].set_title("F_specular")
+    axs[1, 0].plot(Theta_array, F_Doppler)
+    axs[1, 0].set_title("F_Doppler")
+    axs[1, 1].plot(Theta_array, F_ellip)
+    axs[1, 1].set_title("F_ellip")
+    plt.show()
     
-    # plt.subplots()
-    # plt.plot(Theta_array, Fp2Fs(Theta_array, AB, alpha_ellipse, alpha_Doppler))
-    # plt.ylim([0, max(F_thermal + F_specular + F_Doppler + F_ellip)*1.1])
-    # plt.show()
+    plt.subplots()
+    plt.plot(Theta_array, Fp2Fs(Theta_array, AB, alpha_ellipse, alpha_Doppler))
+    plt.ylim([0, max(F_thermal + F_specular + F_Doppler + F_ellip)*1.1])
+    plt.show()
     
